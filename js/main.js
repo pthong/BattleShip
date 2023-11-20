e87d91ec78508e4ca44e69478040bce5a9ced737
ThongZen <pascalthong@gmail.com>
2023-11-20 21:14:09 +0100

    Harbour created. still have a bug :(

--- js/main.js
+++ js/main.js
@@ -1,1 +1,22 @@
 console.log("Game is loaded");
+
+const boatsInHarbour = document.querySelectorAll(".harbour .boat");
+const myField = document.querySelector(".my-field");
+
+let lastClickedBoat;
+
+for (let i = 0; i < boatsInHarbour.length; i++) {
+    console.log(i);
+    const boat = boatsInHarbour[i];
+    boat.addEventListener("click", (event) =>{
+        lastClickedBoat = event.target;
+        console.log(lastClickedBoat);
+    });
+}
+
+
+myField.addEventListener("click", function(event){
+    if(lastClickedBoat){
+        myField.appendChild(lastClickedBoat);
+    }
+});
