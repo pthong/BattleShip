dc8f7dd84e3f20aa509ab911958f773e3660eea8
ThongZen <pascalthong@gmail.com>
2023-11-20 22:42:10 +0100

    iedere place in my-field heeft nu een positie

--- js/main.js
+++ js/main.js
@@ -5,6 +5,8 @@ const myField = document.querySelector(".my-field");
 
 let lastClickedBoat;
 
+initPositions();
+
 for (let i = 0; i < boatsInHarbour.length; i++) {
     console.log(i);
     const boat = boatsInHarbour[i];
@@ -16,32 +18,41 @@ for (let i = 0; i < boatsInHarbour.length; i++) {
 
 
 myField.addEventListener("click", function(event){
-    // console.log("x: ", event.clientX);
-    // console.log("y: ", event.clientY);
     let position = -1;
-
-
-    var searchString = 'position';
 
     event.target.classList.forEach(classe => {
             if(String(classe).startsWith("position")){
-                console.log(classe);
                 position = classe.replace("position", "");
                 console.log(position);
             }
-
     });
 
+    let x,y;
+    x = position%10;
+    y = 2;
 
     console.log(event.target.classList);
-    // console.log(event.target.style.gridArea);
-
     if(lastClickedBoat){
         if(lastClickedBoat.classList.contains("bsm")){
-            lastClickedBoat.style.gridArea = `5 / 2 / span 1/ span 2`;
+            lastClickedBoat.style.gridArea = `5 / ${x} / span 1/ span 2`;
         }else if(lastClickedBoat.classList.contains("bm")){
-            lastClickedBoat.style.gridArea = `5 / 2 / span 1/ span 3`;
+            lastClickedBoat.style.gridArea = `5 / ${x} / span 1/ span 3`;
         }
         myField.appendChild(lastClickedBoat);
     }
 });
+
+
+
+function initPositions(){
+    const myPlaces = document.querySelectorAll(".my-field .place");
+    for (let index = 0; index < myPlaces.length; index++) {
+        const element = myPlaces[index];
+        const position = index+1;
+        let x = index % 10 + 1;//hier is 10 blokken breed (denk ik)
+        let y = position % 10 != 0 ? (position - x) / 10 : (position - x) / 10 ; // hier is 10, 10 blokken hoog. (denk ik)
+        y++;
+        // console.log(`position = ${position}, x = ${x}, y = ${y}`);
+        element.style.gridArea = `${y} / ${x} / span 1/ span 1`;
+    }
+}
